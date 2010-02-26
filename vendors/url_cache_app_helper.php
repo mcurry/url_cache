e5cb53041c3b34f451b5649bd5027f0fcbea72d8
Phally <info@frankdegraaf.net>
2010-02-26 10:25:18 +0100

    Fixed bug with duplicate cache keys, caused by adding the file hash (which resulted in an integer) instead of concatting it.

--- vendors/url_cache_app_helper.php
+++ vendors/url_cache_app_helper.php
@@ -50,7 +50,7 @@ class UrlCacheAppHelper extends Helper {
     }
 
     $key = md5(serialize($keyUrl) . $full);
-    $key += md5_file(CONFIGS . DS . 'routes.php');
+    $key .= md5_file(CONFIGS . DS . 'routes.php');
 
     if (!empty($this->_cache[$key])) {
       return $this->_cache[$key];
